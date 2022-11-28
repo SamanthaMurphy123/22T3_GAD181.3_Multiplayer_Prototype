f4e0140bd0b85079dffd156f64889fbc725b62c0
KhamHuyLu <85721895+KhamHuyLu@users.noreply.github.com>
2022-11-28 23:37:56 +1100

    update

--- Coin_Collector/Assets/Script/CameraFollow.cs
+++ Coin_Collector/Assets/Script/CameraFollow.cs
@@ -2,19 +2,43 @@ using System.Collections;
 using System.Collections.Generic;
 using UnityEngine;
 
+[RequireComponent(typeof(Camera))]
 public class CameraFollow : MonoBehaviour
 {
     public List<Transform> targets;
     public Vector3 offset;
-    public float smoothTime = 5f;
+    public float smoothTime = .5f;
+
+
+    public float minZoom = 40f;
+    public float maxZoom = 10f;
+    public float zoomLimiter = 50f;
 
     private Vector3 velocity;
+    private Camera cam;
+
+    void Start()
+    {
+        cam = GetComponent<Camera>();
+    }
 
     private void LateUpdate()
     {
         if (targets.Count == 0)
             return;
 
+        Move();
+        Zoom();
+    }
+
+    void Zoom()
+    {
+        float newZoom = Mathf.Lerp(maxZoom, minZoom, GetGreatestDistance() / zoomLimiter);
+        cam.fieldOfView = Mathf.Lerp(cam.fieldOfView, newZoom, Time.deltaTime);
+    }
+
+    void Move()
+    {
         Vector3 centerPoint = GetCenterPoint();
 
         Vector3 newPosition = centerPoint + offset;
@@ -22,6 +46,16 @@ public class CameraFollow : MonoBehaviour
         transform.position = Vector3.SmoothDamp(transform.position, newPosition, ref velocity, smoothTime);
     }
 
+    float GetGreatestDistance()
+    {
+        var bounds = new Bounds(targets[0].position, Vector3.zero);
+        for (int i = 0; i < targets.Count; i++)
+        {
+            bounds.Encapsulate(targets[i].position);
+        }
+
+        return bounds.size.x;
+    }
     Vector3 GetCenterPoint()
     {
         if (targets.Count == 1)
