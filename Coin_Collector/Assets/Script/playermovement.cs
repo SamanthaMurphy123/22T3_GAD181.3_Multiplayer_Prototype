eeefd6041fd4792779ca12c5e9bd24e3a4b0d667
KhamHuyLu <85721895+KhamHuyLu@users.noreply.github.com>
2022-11-28 12:59:54 +1100

    test

---
+++ Coin_Collector/Assets/Script/playermovement.cs
@@ -0,0 +1,37 @@
+using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+
+public class playermovement : MonoBehaviour
+{
+    [SerializeField]
+    private float speed;
+    private Rigidbody2D body;
+
+    public Transform groundCheck;
+    public float groundCheckRadius;
+    public LayerMask groundLayer;
+    private bool isTouchingGround;
+
+    private void Awake()
+    {
+        body = GetComponent<Rigidbody2D>();
+    }
+
+    private void Update()
+    {
+        isTouchingGround = Physics2D.OverlapCircle(groundCheck.position, groundCheckRadius, groundLayer);
+        float horizontalInput = Input.GetAxis("Horizontal");
+        body.velocity = new Vector2(horizontalInput * speed, body.velocity.y);
+
+
+        // flip
+        if (horizontalInput > 0.01f)
+            transform.localScale = Vector3.one;
+        else if (horizontalInput < -0.01f)
+            transform.localScale = new Vector3(-1, 1, 1);
+
+        if (Input.GetKey(KeyCode.Space) && isTouchingGround)
+            body.velocity = new Vector2(body.velocity.x, speed);
+    }
+}
