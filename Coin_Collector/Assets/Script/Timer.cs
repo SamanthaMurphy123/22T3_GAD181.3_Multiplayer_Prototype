ec42cc6d5927f842c6c0244029be360f337726e8
KhamHuyLu <85721895+KhamHuyLu@users.noreply.github.com>
2022-12-05 23:21:35 +1100

    update

---
+++ Coin_Collector/Assets/Script/Timer.cs
@@ -0,0 +1,43 @@
+using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+using UnityEngine.UI;
+using TMPro;
+
+public class Timer : MonoBehaviour
+{
+    [SerializeField] private Image uiFill;
+    [SerializeField] private TextMeshPro uiText;
+
+    public int Duration;
+
+    private int remainingDuration;
+
+    private void Start()
+    {
+        Being(Duration);
+    }
+
+    private void Being(int Second)
+    {
+        remainingDuration = Second;
+        StartCoroutine(UpadateTimer());
+    }
+
+    private IEnumerator UpadateTimer()
+    {
+        while(remainingDuration>=0)
+        {
+            uiText.text = $"{remainingDuration / 60:00}:{remainingDuration % 60:00}";
+            uiFill.fillAmount = Mathf.InverseLerp(0, Duration, remainingDuration);
+            remainingDuration--;
+            yield return new WaitForSeconds(1f);
+        }
+        OnEnd();
+    }
+
+    private void OnEnd()
+    {
+
+    }
+}
