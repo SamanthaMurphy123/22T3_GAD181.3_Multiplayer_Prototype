29a530cca81a7909c6e2d18ac3f5478963d6288d
KhamHuyLu <85721895+KhamHuyLu@users.noreply.github.com>
2022-12-06 14:42:14 +1100

    update

--- Coin_Collector/Assets/Script/CoinPiker.cs
+++ Coin_Collector/Assets/Script/CoinPiker.cs
@@ -5,50 +5,20 @@ using TMPro;
 
 public class CoinPiker : MonoBehaviour
 {
-    public float Coin1 = 0;
-    public float Coin2 = 0;
+    // The number of coins that the picker has picked up
+    public int coins = 0;
 
-    public TextMeshProUGUI textCoin;
+    // The text element that will display the number of coins picked up
+    public TextMeshProUGUI textCoins;
 
-    public AudioClip coinSound;
-
-    public GameObject player1;
-    public GameObject player2;
-
-    [SerializeField] 
-
-    private void OnTriggerEnter2D(Collider2D other)
+    void OnTriggerEnter2D(Collider2D other)
     {
-        if(other.transform.tag == "Coin" && gameObject == player1)
+        // If the picker collides with a coin, pick it up
+        if (other.gameObject.CompareTag("Coin"))
         {
-            Coin1++;
-            textCoin.text = Coin1.ToString();
-
-            AudioSource.PlayClipAtPoint(coinSound, transform.position,1);
+            coins++;
+            textCoins.text = coins.ToString();
             Destroy(other.gameObject);
         }
-
-        if (other.transform.tag == "Coin" && gameObject == player2)
-        {
-            Coin2++;
-            textCoin.text = Coin2.ToString();
-
-            AudioSource.PlayClipAtPoint(coinSound, transform.position, 1);
-            Destroy(other.gameObject);
-
-        }
-        
-
-        
-        
     }
-
-    //public void WinCondition()
-    //{
-    //    if ()
-    //    {
-
-    //    }
-
-    //}
 }
